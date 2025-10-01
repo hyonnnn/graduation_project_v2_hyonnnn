5dc28420c5c0b8ec49dd4488eed1035a998fbc0a
前野彩音 <maenoayane@maenoayanenoMacBook-Air.local>
2025-10-02 01:53:26 +0900

    Fix Devise authentication callbacks for production

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,23 +1,35 @@
-class UserSessionsController < ApplicationController
-  # :require_login は存在しないので raise: false を付けるか、削除
-  # skip_before_action :require_login, raise: false
-  skip_before_action :authenticate_user!, only: [:new, :create]  # ログインページは認証不要
+class UsersController < ApplicationController
+  # 新規登録は認証不要
+  skip_before_action :authenticate_user!, only: [:new, :create]
 
-  def new; end
+  # ユーザー一覧（ログイン必須）
+  def index
+    @users = User.all
+  end
 
+  # ユーザー詳細（ログイン必須）
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # 新規ユーザー登録ページ
+  def new
+    @user = User.new
+  end
+
+  # ユーザー作成処理
   def create
-    @user = login(params[:email], params[:password])
-
-    if @user
-      redirect_to(root_path, notice: 'Login successful')
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'User created successfully'
     else
-      flash.now[:alert] = 'Login failed'
       render :new
     end
   end
 
-  def destroy
-    logout
-    redirect_to(root_path, notice: 'Logged out')
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
