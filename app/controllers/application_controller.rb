afa5754191267e8adc56369fa78ab6e02f4df20f
前野彩音 <maenoayane@maenoayanenoMacBook-Air.local>
2025-10-02 14:28:27 +0900

    ログアウト後の遷移先をトップページに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  # ログイン後の遷移先をタスク一覧に変更
+  # ログイン後の遷移先をタスク一覧に設定
   def after_sign_in_path_for(resource)
     tasks_path
+  end
+
+  # ログアウト後の遷移先をホームページ（トップページ）に設定
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
   end
 end
